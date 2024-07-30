a51926b0e48708fdbf32c757d19f0ea51674691a
Brian <smiledev10162@gmail.com>
2024-07-30 09:54:05 +0700

    update header as fixed

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -3,36 +3,36 @@
 
   // ======= Sticky
   window.onscroll = function () {
-    // const ud_header = document.querySelector(".ud-header");
-    // const sticky = ud_header.offsetTop;
-    // const logo = document.querySelectorAll(".header-logo");
-    //
-    // if (window.pageYOffset > sticky) {
-    //   ud_header.classList.add("sticky");
-    // } else {
-    //   ud_header.classList.remove("sticky");
-    // }
-    //
-    // if(logo.length) {
-    //   // === logo change
-    //   if (ud_header.classList.contains("sticky")) {
-    //     document.querySelector(".header-logo").src =
-    //       "assets/images/logo/logo.svg"
-    //   } else {
-    //     document.querySelector(".header-logo").src =
-    //       "assets/images/logo/logo-white.svg"
-    //   }
-    // }
-    //
-    // if (document.documentElement.classList.contains("dark")) {
-    //   if(logo.length) {
-    //     // === logo change
-    //     if (ud_header.classList.contains("sticky")) {
-    //       document.querySelector(".header-logo").src =
-    //         "assets/images/logo/logo-white.svg"
-    //     }
-    //   }
-    // }
+    const ud_header = document.querySelector(".ud-header");
+    const sticky = ud_header.offsetTop;
+    const logo = document.querySelectorAll(".header-logo");
+
+    if (window.pageYOffset > sticky) {
+      ud_header.classList.add("sticky");
+    } else {
+      ud_header.classList.remove("sticky");
+    }
+
+    if (logo.length) {
+      // === logo change
+      if (ud_header.classList.contains("sticky")) {
+        document.querySelector(".header-logo").src =
+          "assets/images/logo/logo.svg";
+      } else {
+        document.querySelector(".header-logo").src =
+          "assets/images/logo/logo-white.svg";
+      }
+    }
+
+    if (document.documentElement.classList.contains("dark")) {
+      if (logo.length) {
+        // === logo change
+        if (ud_header.classList.contains("sticky")) {
+          document.querySelector(".header-logo").src =
+            "assets/images/logo/logo-white.svg";
+        }
+      }
+    }
 
     // show or hide the back-top-top button
     const backToTop = document.querySelector(".back-to-top");
@@ -66,11 +66,17 @@
     );
 
   // ===== Sub-menu
-  const submenuItems = document.querySelectorAll(".submenu-item");
-  submenuItems.forEach((el) => {
-    el.querySelector("a").addEventListener("click", () => {
-      el.querySelector(".submenu").classList.toggle("hidden");
-    });
+  // const submenuItems = document.querySelectorAll(".submenu-item");
+  // submenuItems.forEach((el) => {
+  //   console.log(submenuItems);
+  //   el.querySelector("a").addEventListener("click", () => {
+  //     el.querySelector(".submenu").classList.toggle("hidden");
+  //   });
+  // });
+
+  let serviceItem = document.querySelector("#serviceToggle");
+  serviceItem.addEventListener("click", () => {
+    serviceItem.querySelector("#serviceModal").classList.toggle("hidden");
   });
 
   // ===== Faq accordion
